19c98e80ccf44b3a58d2b79477ce5c65daf95023
gregsypek <47268138+gregsypek@users.noreply.github.com>
2023-11-14 18:14:08 +0100

    test new approach

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middleware = jsonServer.defaults();
+// const port = process.env.PORT || 3000;
+
+// server.use(middleware);
+// server.use(router);
+// server.listen(port);
+const express = require("express");
 const jsonServer = require("json-server");
-const server = jsonServer.create();
+
+const app = express();
+const jsonServerApp = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middleware = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-server.use(middleware);
-server.use(router);
-server.listen(port);
+// Use the JSON Server middleware
+jsonServerApp.use(middleware);
+jsonServerApp.use(router);
+
+// Define additional Express endpoints
+app.get("/api/pizzas", (req, res) => {
+	const pizzas = require("./db.json").pizzas;
+	res.json(pizzas);
+});
+
+app.get("/api/menu", (req, res) => {
+	const pizzas = require("./db.json").pizzas;
+	res.json({ data: pizzas });
+});
+
+// Mount the JSON Server app under the /api path
+app.use("/api", jsonServerApp);
+
+// Start the server
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
